a823e76f0886b766fca175933caa55e814f17b69
Hisham Atef <heshamatef040@gmail.com>
2024-04-19 11:34:03 +0200

    add FeaturedListViewItem

--- lib/features/home/presentation/views/widgets/custom_listview_item.dart
+++ lib/features/home/presentation/views/widgets/custom_listview_item.dart
@@ -1,24 +1,22 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: AspectRatio(
-        //نسبة الطول والعرض بالنسبة للصورة
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: DecorationImage(
-                  image: AssetImage(AssetsData.test_book1),
-                  fit: BoxFit.fill
-              )
-          ),
+    return  AspectRatio(
+      //نسبة الطول والعرض بالنسبة للصورة
+      aspectRatio: 3 / 5,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red,
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
+                image: AssetImage(AssetsData.test_book1),
+                fit: BoxFit.fill
+            )
         ),
       ),
     );
