d618d81ed5a189b67b3477ef8281553716a69f66
Hisham Atef <heshamatef040@gmail.com>
2024-04-22 14:04:10 +0200

    finish best seller item ui

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,9 @@
-import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_appbar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/custom_listview_item.dart';
 import 'package:bookly/features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -23,50 +23,14 @@ class HomeViewBody extends StatelessWidget {
           padding: EdgeInsets.only(left: 24),
           child: Text("Best Seller",style: Styles.textStyle18,),
         ),
-        SizedBox(height: 16,),
+        SizedBox(height: 20,),
         BestSellerListViewItem(),
       ],
     );
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: SizedBox(
-        height: 130,
-        child: Row(
-          children:  [
-            AspectRatio(
-              //نسبة الطول والعرض بالنسبة للصورة
-              aspectRatio: 3.5 / 5,
-              child: Container(
-                margin:  const EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                    image: const DecorationImage(
-                        image: AssetImage(AssetsData.test_book1),
-                        fit: BoxFit.fill
-                    )
-                ),
-              ),
-            ),
-            Column(
-              children: [
-
-              ],
-            ),
-          ]
-        ),
-      ),
-    );
-  }
-}
 
 
 
