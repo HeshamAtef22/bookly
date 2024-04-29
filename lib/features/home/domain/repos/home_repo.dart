b53f2fd0e13ecf4cab035432aedc13eedd6f38b5
Hisham Atef <heshamatef040@gmail.com>
2024-04-29 10:00:36 +0300

    add either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,12 @@
+import 'package:bookly/core/errors/failure.dart';
 import 'package:bookly/features/home/domain/entities/book_entitiy.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo
 {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestSellers();
+  //بدل من استخدام ال Try-catch لهندلة الايرور هنستخدم Either الموجوده في باكيدج dartz
+  //واللي مهمتها انها ترجعلي حالتين للداتا في حالة كانت ترو وبدون مشكله R
+  //وفي حالة كانت فولس وبها مشكلة L
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<BookEntity>>> fetchNewestSellers();
 }
