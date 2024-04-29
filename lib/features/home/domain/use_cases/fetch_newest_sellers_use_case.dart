6926b66f31f8664382574405495f2a7d295bab26
Hisham Atef <heshamatef040@gmail.com>
2024-04-29 14:40:20 +0300

    create fetch newest sellers Use Case

---
+++ lib/features/home/domain/use_cases/fetch_newest_sellers_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_case.dart';
+import 'package:bookly/features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class fetchNewestSellers extends UseCase<List<BookEntity>,void> {
+  final HomeRepo homeRepo;
+  fetchNewestSellers(this.homeRepo);
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void parm]) async {
+    return await homeRepo.fetchNewestSellers();
+  }
+}
