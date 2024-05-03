61a4dfbc35933c52b9a4c272db5194fce7066818
Hisham Atef <heshamatef040@gmail.com>
2024-05-03 18:04:35 +0300

    create ServerFailure class and add error cases

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,42 @@
-class Failure {}
+//عن طريق الكلاس دا هبدأ اهندل اليرور اللي بيرجع لو كان مثلا مشكله في الانترنت او السيرفر او غيرها
+//وممكن اظهر لليوزر في كل نوع ايرور رسالة او اكشن معين
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+//كلاس للتعامل مع مشاكل السيرفر
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioError error){
+    switch (error.type){
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate with ApiServer');
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to ApiServer was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection error with ApiServer');
+      case DioExceptionType.unknown:
+        return ServerFailure('Opps There was an error. Please try again');
+      case DioExceptionType.badResponse:
+      // TODO: Handle this case.
+    }
+  }
+  
+}
+
+
+//كلاس للتعامل مع مشاكل الكاش
+//class CacheFailure extends Failure {}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:bookly/features/home/data/data_sources/home_remote_data_source.d
 import 'package:bookly/features/home/domain/entities/book_entitiy.dart';
 import 'package:bookly/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 //HemoRepoImpl مهمته انه يجمع او يعمل فيتش للداتامن ال اي بي اي او من الكاش لو الداتا موجوده في الكاش
 class HemoRepoImpl extends HomeRepo{
@@ -20,23 +21,37 @@ class HemoRepoImpl extends HomeRepo{
 
     try {
       //في الاول هخليه يتأكد لو في داتا في الكاش يستخدمها ولو الكاش فاضي فهيعمل fetch للداتا من ال api
-      var booksList = homeLocalDataSource.fetchFeaturedBooks();
+      List<BookEntity> booksList = homeLocalDataSource.fetchFeaturedBooks();
       if(booksList.isNotEmpty){
         return right(booksList);
       }
       //هعمل متغير احتفظ فيه بالداتا اللي هترجع من ال RemoteDataSource
-      var books = await homeRemoteDataSource.fetchFeaturedBooks();
+      List<BookEntity> books = await homeRemoteDataSource.fetchFeaturedBooks();
       //في حالة ان القيمة هترجع بدون اي مشاكل فرجعلي الجزء الr الخاص بال Either غير كدا لو الداتا فيها مشكله رجعلي الجزء الl
       return right(books) ;
     }  catch (e) {
-      return left(Failure());
+      if(e is DioError){
+        return left(ServerFailure.fromDioError(e));
+      }
+
     }
   }
 
   @override
-  Future<Either<Failure, List<BookEntity>>> fetchNewestSellers() {
-    // TODO: implement fetchNewestSellers
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestSellers() async{
+    try {
+      //في الاول هخليه يتأكد لو في داتا في الكاش يستخدمها ولو الكاش فاضي فهيعمل fetch للداتا من ال api
+      List<BookEntity> booksList = homeLocalDataSource.fetchNewestSellers();
+      if(booksList.isNotEmpty){
+        return right(booksList);
+      }
+      //هعمل متغير احتفظ فيه بالداتا اللي هترجع من ال RemoteDataSource
+      List<BookEntity> books = await homeRemoteDataSource.fetchNewestSellers();
+      //في حالة ان القيمة هترجع بدون اي مشاكل فرجعلي الجزء الr الخاص بال Either غير كدا لو الداتا فيها مشكله رجعلي الجزء الl
+      return right(books) ;
+    }  catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 
 }
